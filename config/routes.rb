7f5fed2acbaa35fc73b79ebc9e01239046279f62
Jon Evans <jle@foraker.com>
2014-11-06 00:34:53 -0700

    Add basic navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'sessions#new'
 
   resources :sessions, only: [:new, :create, :destroy]
+  delete "signout" => "sessions#destroy", as: "signout"
   resources :pull_requests, only: :index
   resources :comments, only: :index
 end
